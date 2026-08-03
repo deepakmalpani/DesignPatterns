a463488eeae3a3557ed1a7fa22e026ba24ee34b1
Deepak Malpani <deepakmalpani0@gmail.com>
2026-08-03 12:11:17 +0530

    Refactor Logger class to simplify Instance property and ensure thread safety

--- Singleton/Logger.cs
+++ Singleton/Logger.cs
@@ -1,16 +1,10 @@
 public sealed class Logger
 {
-    private static volatile Logger instance = new Logger();
+    private static readonly Logger instance = new Logger();
 
     private Logger() { }
 
-    public static Logger Instance
-    {
-        get
-        {
-            return instance;
-        }
-    }
+    public static Logger Instance => instance;
 
     public void Log(string log)
     {
